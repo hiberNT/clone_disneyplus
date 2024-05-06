3df94eed65db56fa7e552deb40dadca45628aaee
hibernon rodrigues <hibernon73@gmail.com>
2024-05-06 19:36:26 -0300

    criando o hero e criando a fonte avenir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() { //estamos fazendo uma seçao para resgatar os estilos aplicados no scss 
     return gulp.src('./src/styles/*.scss')  //acessando a pasta src q tem o arq styles e usamos * para que seja resgatado tudo q tenha scss 
@@ -7,8 +8,15 @@ function styles() { //estamos fazendo uma seçao para resgatar os estilos aplica
         .pipe(gulp.dest('./dist/css'));//e aqui é a pasta de destino que vai os arquivos comprimidos acima 
 }
 
-exports.default = styles
+function images() {//MINIFICANDO FOTOS (faço depois de ter feito o gulp,sass e wacth)
+    return gulp.src('./src/images/**/*',{ encoding: false })  //esta acesseando a pasta images e tbm as outras pastas q existem dentro de images
+        .pipe(imagemin())//esse { encoding: false }) coloquei pq a versao do gulp n esta minificando as imagens todas e com esse comando da certo
+        .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles,images)//antes de expotar as images estava assim pois so exsitia syles -> exports.default = styles
 //npm run buid para testar e criar a pasta dist
+
 exports.watch = function() {//apos o comanado de cima venho pra ca
     gulp.watch('./src/styles/*main.scss',gulp.parallel(styles))//estamos colocando pra observar quando tiver mudança nos arquivos scss
 }
